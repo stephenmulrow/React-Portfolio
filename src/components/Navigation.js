fab0c214e189bbdb59f4b20de35af89bd82a68b0
smulrow <smulrow@gmail.com>
2022-09-02 15:11:14 -0700

    changing structure of portfolio container

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -14,35 +14,85 @@ const styles = {
   }
 }
 
-export default function Navigation(){
-  return(
-    <main>
-      <nav class="navbar navbar-expand-lg" id="header-bg">
-  <div class="container-fluid" id="navList">
-    <a class="navbar-brand" href="#"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <p className="p" style={styles.p}>Stephen Mulrow
-        <footer class="blockquote-footer" className="subP" style={styles.subP}>Full-Stack Developer</footer></p>
-      <ul class="navbar-nav" id="navList">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/React-Portfolio">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#portfolio">Projects</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#resume">Resume</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#contact">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-    </main>
-  )
+function Navigation({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#project"
+          onClick={() => handlePageChange('Project')}
+          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Project' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#resume"
+          onClick={() => handlePageChange('Resume')}
+          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
 }
+
+export default Navigation;
+
+// export default function Navigation(){
+//   return(
+//     <main>
+//       <nav class="navbar navbar-expand-lg" id="header-bg">
+//   <div class="container-fluid" id="navList">
+//     <a class="navbar-brand" href="#"></a>
+//     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//       <span class="navbar-toggler-icon"></span>
+//     </button>
+//     <div class="collapse navbar-collapse" id="navbarNav">
+//       <p className="p" style={styles.p}>Stephen Mulrow
+//         <footer class="blockquote-footer" className="subP" style={styles.subP}>Full-Stack Developer</footer></p>
+//       <ul class="navbar-nav" id="navList">
+//         <li class="nav-item">
+//           <a class="nav-link active" aria-current="page" href="/React-Portfolio">Home</a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link" href="#portfolio">Projects</a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link" href="#resume">Resume</a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link" href="#contact">Contact</a>
+//         </li>
+//       </ul>
+//     </div>
+//   </div>
+// </nav>
+//     </main>
+//   )
+// }
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Footer from "./pages/Footer"
 import Navigation from "./Navigation"
 import Project from "./pages/Project"
@@ -9,52 +9,80 @@ import Contact from "./pages/Contact";
 
 
 export default function PortfolioContainer() {
-  
-  return (
+  const [currentPage, setCurrentPage] = useState('About');
+
+
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Project') {
+      return <Project />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+  }
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return(
     <div>
-      {/* We are passing the currentPage from state and the function to update it */}
-      <Router>
-      <Navigation />
-        <div className="flex-column justify-flex-start min-100-vh" id="header">
-          
-          <div className="container">
-            {/* Wrap Route elements in a Routes component */}
-            <Routes>
-              {/* Define routes using the Route component to render different page components at different paths */}
-              {/* Define a default route that will render the Home component */}
-              <Route 
-                exact path="/" 
-                element={<About />} 
-              />
-              <Route 
-                path="/about" 
-                element={<About />} 
-              />
-              {/* <Route 
-                path="/React-Portfolio" 
-                element={<About />} 
-              /> */}
-              {/* Define a route that will take in variable data */}
-               <Route 
-                path="/contact" 
-                element={<Contact />} 
-              />
-              <Route
-                path="/portfolio"
-                element={<Project />}
-                />
-              <Route
-                path="/resume"
-                element={<Resume />}
-                /> 
-            </Routes>
-          </div>
-          
-        </div>
-      <Footer />
-      </Router>
-      {/* Herewe are calling the renderPage method which will return a component  */}
-      
+      <Navigation currentPage={currentPage}
+      handlePageChange={handlePageChange} />
+      {renderPage()}
     </div>
-  );
+  )
 }
+//   )
+//   return (
+//     <div>
+//       {/* We are passing the currentPage from state and the function to update it */}
+//       <Router>
+//       <Navigation />
+//         <div className="flex-column justify-flex-start min-100-vh" id="header">
+          
+//           <div className="container">
+//             {/* Wrap Route elements in a Routes component */}
+//             <Routes>
+//               {/* Define routes using the Route component to render different page components at different paths */}
+//               {/* Define a default route that will render the Home component */}
+//               <Route 
+//                 exact path="/" 
+//                 element={<About />} 
+//               />
+//               <Route 
+//                 path="/about" 
+//                 element={<About />} 
+//               />
+//               {/* <Route 
+//                 path="/React-Portfolio" 
+//                 element={<About />} 
+//               /> */}
+//               {/* Define a route that will take in variable data */}
+//                <Route 
+//                 path="/contact" 
+//                 element={<Contact />} 
+//               />
+//               <Route
+//                 path="/portfolio"
+//                 element={<Project />}
+//                 />
+//               <Route
+//                 path="/resume"
+//                 element={<Resume />}
+//                 /> 
+//             </Routes>
+//           </div>
+          
+//         </div>
+//       <Footer />
+//       </Router>
+//       {/* Herewe are calling the renderPage method which will return a component  */}
+      
+//     </div>
+//   );
+// }
